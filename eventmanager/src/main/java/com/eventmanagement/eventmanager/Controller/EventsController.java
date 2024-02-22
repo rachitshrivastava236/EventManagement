fe46d65ad031d2f389c2a0fb47e4159f73e3a73b
Rachit <rs.phantom236@gmail.com>
2024-02-22 23:58:51 +0530

    Update to Event Controller Autowire

--- eventmanager/src/main/java/com/eventmanagement/eventmanager/Controller/EventsController.java
+++ eventmanager/src/main/java/com/eventmanagement/eventmanager/Controller/EventsController.java
@@ -2,8 +2,10 @@ package com.eventmanagement.eventmanager.Controller;
 
 import com.eventmanagement.eventmanager.Entity.Events;
 import com.eventmanagement.eventmanager.Service.EventsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.parser.Entity;
@@ -12,8 +14,10 @@ import java.util.List;
 @RestController
 public class EventsController {
 
+    @Autowired
     private EventsService eventsService;
 
+    @GetMapping(path = "/getAllEvents")
     public ResponseEntity<List<Events>> getAllEvents(){
         return  ResponseEntity.status(HttpStatus.OK).body(eventsService.getEvents());
     }
