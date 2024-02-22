b57b7aa85de37d1ecde654475bdf02f0e1109ae4
Rachit <rs.phantom236@gmail.com>
2024-02-22 22:25:54 +0530

    Created User Entity

---
+++ eventmanager/src/main/java/com/eventmanagement/eventmanager/Entity/Users.java
@@ -0,0 +1,25 @@
+package com.eventmanagement.eventmanager.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String email;
+    private String role;
+    private String password;
+
+}
